c78c66bb3a75599f41203768d005cc0eaf4d2940
MOHIT B <mbcse50@gmail.com>
2020-06-22 15:20:11 +0530

    Few changes

---
+++ src/tendering/app/app.py
@@ -0,0 +1,237 @@
+from flask import Flask, render_template, request, send_file, flash, redirect, session, abort
+import re
+import pyrebase
+
+app = Flask(__name__)
+
+config = {
+ "apiKey": "AIzaSyCvk5pT2v7tH5VmqkgLhb9iRDh6UOYSRsg",
+    "authDomain": "tender-3d87b.firebaseapp.com",
+    "databaseURL": "https://tender-3d87b.firebaseio.com",
+    "projectId": "tender-3d87b",
+    "storageBucket": "tender-3d87b.appspot.com",
+    "messagingSenderId": "887633995621",
+    "appId": "1:887633995621:web:e1ea60e72718e127d3a38c",
+    "measurementId": "G-RKSZNH2M7P"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	if session.get('logged_in') or session.get('dealer_logged_in'):
+		return render_template('index.html', user=session['user'])
+	else:
+		return render_template('index.html')
+
+@app.route('/demo', methods=['GET', 'POST'])
+def demo():
+	if session.get('logged_in') or session.get('dealer_logged_in'):
+		return render_template('demo.html', user=session['user'])
+	else:
+		return render_template('demo.html')
+
+@app.route('/tender/<tender_title>', methods=['GET', 'POST'])
+def this_tender(tender_title):
+	accs = db.child("tenders").child('all_tenders').get()
+	reqel = 1
+	for idx in accs.each():
+		try:
+			if(idx.val()['org'] == tender_title):
+				reqel = idx.val()
+				myqel = idx.key()
+		except KeyError:
+			print("foo")
+
+	accs2 = db.child("bidders").child('confirmed').get()
+	checker = -1
+	for idx in accs2.each():
+		try:
+			if(idx.val()[session['user']] == "confirmed" or idx.val()[session['user']] == "failed"):
+				xeqel = idx.val()
+				xmyqel = idx.key()
+				checker = 1
+				break
+		except KeyError:
+			print("foo")
+
+	tender_details = []
+	print(reqel)
+	tender_details.extend([reqel['bidclose'], reqel['bidop'], reqel['bidopen'], reqel['clare'], reqel['clars'], reqel['downde'], reqel['downds'], reqel['financedetails'], reqel['formcont'], reqel['gentech'], reqel['iem'], reqel['meetadd'], reqel['meetdate'], reqel['mulca'], reqel['org'], reqel['paymode'], reqel['payto'], reqel['pera'], reqel['pername'], reqel['preq'], reqel['pubd'], reqel['techdetails'], reqel['tendercat'], reqel['tenderid'], reqel['tenderref'], reqel['tenderstatus'], reqel['tenderval'], reqel['tfe'], reqel['titlec'], reqel['workd']])
+
+	try:
+		if(reqel[session['user']] != "vzxjhjxhcjxhuhxxx"):
+			print("yay")
+	except KeyError:
+		db.child("tenders").child("all_tenders").child(myqel).update({session['user']: "false"})
+		allownow = True
+
+	if(session.get('logged_in') and checker == -1):
+		db.child("bidders").child("confirmed")
+		db.push({session['user']: "failed"})
+		return render_template('registerdetails.html', user=session['user'])
+
+	if(session.get('logged_in') and xeqel[session['user']] != "confirmed"):
+		return render_template('registerdetails.html', user=session['user'])
+
+	if request.method == 'POST' and 'bidamt' in request.form:
+		db.child("tenders").child("all_tenders").child(myqel).update({session['user']: "true"})
+		return render_template('tender.html', user=session['user'], data=tender_details, isOpen="true", hasBid="true")
+
+	if request.method == 'POST':
+		print(myqel)
+		db.child("tenders").child("all_tenders").child(myqel).update({"tenderstatus": "false"})
+		return render_template('tender.html', user=session['user'], data=tender_details, isOpen="false")
+
+	if session.get('logged_in') and (reqel['tenderstatus'] == "true") and (reqel[session['user']] == "true"):
+		return render_template('tender.html', user=session['user'], data=tender_details, isOpen="true", hasBid="true")
+
+	if session.get('logged_in') and (reqel['tenderstatus'] == "true") and (reqel[session['user']] == "false"):
+		return render_template('tender.html', user=session['user'], data=tender_details, isOpen="true", hasBid="false")
+
+	if session.get('logged_in') and (reqel['tenderstatus'] == "true") and allownow == True:
+		return render_template('tender.html', user=session['user'], data=tender_details, isOpen="true", hasBid="false")
+
+	if session.get('dealer_logged_in') and (reqel['tenderstatus'] == "true"):
+		return render_template('tender.html', user=session['user'], data=tender_details, isOpen="true")
+
+	if session.get('logged_in') and (reqel['tenderstatus'] != "true"):
+		return render_template('tender.html', user=session['user'], data=tender_details, isOpen="false")
+
+	if session.get('dealer_logged_in') and (reqel['tenderstatus'] != "true"):
+		return render_template('tender.html', user=session['user'], data=tender_details, isOpen="false")
+
+	return render_template('login.html')
+
+@app.route('/userpage', methods=['GET', 'POST'])
+def userpage():
+	if session.get('logged_in'):
+		return render_template('userpage.html', username=session['user'])
+	else:
+		return render_template('index.html')
+
+@app.route('/addTender', methods=['GET', 'POST'])
+def addTender():
+	if request.method == 'POST':
+		db.child("tenders").child("all_tenders")
+		data = {"org" : request.form['org'], "tenderref" : request.form['tenderref'], "tenderid" : request.form['tenderid'], "tenderstatus" : request.form['tenderstatus'], "tendercat" : request.form['tendercat'], "techdetails" : request.form['techdetails'], "financedetails" : request.form['financedetails'], "bidopen" : request.form['bidopen'], "bidclose" : request.form['bidclose'], "tenderval" : request.form['tenderval'], "paymode" : request.form['paymode'], "payto" : request.form['payto'], "gentech" : request.form['gentech'], "formcont" : request.form['formcont'], "titlec" : request.form['titlec'], "workd" : request.form['workd'], "preq" : request.form['preq'], "meetadd" : request.form['meetadd'], "meetdate" : request.form['meetdate'], "bidop" : request.form['bidop'], "pubd" : request.form['pubd'], "downds" : request.form['downds'], "downde" : request.form['downde'], "clars" : request.form['clars'], "clare" : request.form['clare'], "iem" : request.form['iem'], "tfe" : request.form['tfe'], "pername" : request.form['pername'], "pera" : request.form['pera'], "mulca" : request.form['mulca']}
+		db.push(data)
+		return render_template('index.html', username=session['user'])
+
+	if session.get('dealer_logged_in'):
+		return render_template('addTender.html', username=session['user'])
+	else:
+		return render_template('index.html')
+
+@app.route('/portal', methods=['GET', 'POST'])
+def portal():
+	if session.get('logged_in') or session.get('dealer_logged_in'):
+		return render_template('portal.html', user=session['user'])
+	else:
+		return render_template('portal.html')
+
+@app.route('/whitepaper', methods=['GET', 'POST'])
+def whitepaper():
+	if session.get('logged_in') or session.get('dealer_logged_in'):
+		return render_template('whitepaper.html', user=session['user'])
+	else:
+		return render_template('whitepaper.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST' and 'dealerusername' in request.form and 'dealerpassword' in request.form:
+		accs = db.child("accounts").child('supplier').get()
+		for idx in accs.each():
+			try:
+				if(idx.val()[request.form['dealerusername']] == request.form['dealerpassword']):
+					session['dealer_logged_in'] = True
+					session['user'] = request.form['dealerusername']
+					return render_template('index.html', user=session['user'])
+			except KeyError:
+				print("foo")
+
+	if request.method == 'POST' and 'contractorusername' in request.form and 'contractorpassword' in request.form:
+		accs = db.child("accounts").child('clients').get()
+		for idx in accs.each():
+			try:
+				if(idx.val()[request.form['contractorusername']] == request.form['contractorpassword']):
+					session['logged_in'] = True
+					session['user'] = request.form['contractorusername']
+					return render_template('index.html', user=session['user'])
+			except KeyError:
+				print("foo")
+
+	return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+	session['logged_in'] = False
+	session['dealer_logged_in'] = False
+	return index()
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if session.get('logged_in') or session.get('dealer_logged_in'):
+		return render_template('register.html', user=session['user'])
+
+	if request.method == 'POST' and 'contractorusername' in request.form and 'contractorpassword' in request.form:
+		db.child("accounts").child("clients")
+		data = {request.form['contractorusername'] : request.form['contractorpassword']}
+		db.push(data)
+		session['user'] = request.form['contractorusername']
+
+		db.child("bidders").child("confirmed")
+		dat = {session['user'] : "failed"}
+		db.push(dat)
+
+		return render_template('registerdetails.html', user=session['user'])
+	else:
+		flash('wrong password!')
+
+	if request.method == 'POST' and 'dealerusername' in request.form and 'dealerpassword' in request.form:
+		db.child("accounts").child("supplier")
+		dat = {request.form['dealerusername'] : request.form['dealerpassword']}
+		db.push(dat)
+		session['user'] = request.form['dealerusername']
+		session['dealer_logged_in'] = True
+		return render_template('index.html', user=session['user'])
+	else:
+		flash('wrong password!')
+
+	return render_template('register.html')
+
+@app.route('/registerdetails', methods=['GET', 'POST'])
+def registerdetails():
+
+	accs = db.child("bidders").child('confirmed').get()
+	for idx in accs.each():
+		try:
+			if(idx.val()[session['user']] == "failed"):
+				thisone = idx.key()
+		except KeyError:
+			print("foo")
+
+	if request.method == 'POST':
+		db.child("bidders").child("confirmed").child(thisone).update({session['user']: "confirmed"})
+		session['logged_in'] = True
+		print(thisone)
+		return render_template('index.html', user=session['user'])
+
+	return render_template('registerdetails.html', user=session['user'])
+
+@app.route('/tender', methods=['GET', 'POST'])
+def tender():
+	return render_template("index.html", user=session['user'])
+
+@app.route('/developers', methods=['GET', 'POST'])
+def developers():
+	if session.get('logged_in') or session.get('dealer_logged_in'):
+		return render_template('developers.html', user=session['user'])
+	else:
+		return render_template('developers.html')
+
+app.secret_key = "helloworld this is satoshi"
+
+if __name__ == '__main__':
+	app.run(debug=True)
---
+++ src/tendering/smart-contract/truffle.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+
+  compilers: {
+    solc: {
+      version: "0.4.17" // ex:  "0.4.20". (Default: Truffle's installed solc)
+    }
+ } 
+};
