1442a1be8485748454de43ff49a55b63283920a9
MOHIT B <mbcse50@gmail.com>
2020-06-29 02:25:04 +0530

    Initialising Directory Structure

--- migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-var UserRegistry = artifacts.require("./UserRegistry.sol");
-var OrderRegistry = artifacts.require("./OrderRegistry.sol");
-module.exports = function(deployer) {
-  deployer.deploy(UserRegistry);
-  deployer.deploy(OrderRegistry);
-};
